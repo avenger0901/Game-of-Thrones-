94c669556c65ef3a354e789309d5551d5387d892
zhao ziyi <zhaoziy@oregonstate.edu>
2020-01-30 13:17:28 -0800

    render link

--- map/map.js
+++ map/map.js
@@ -1,4 +1,20 @@
 import loadProfile from '../utils/loadProfile.js';
-
+import quests from '../data/questData.js';
 loadProfile();
 
+function renderLink(quest){
+    const nav = document.getElementById('nav');
+    const link = document.createElement('a');
+    console.log(link);
+    link.classList.add('quest');
+    nav.appendChild(link);
+    link.textContent = quest.title;
+    link.href = '../quest/?id=' + quest.id;
+
+    return link;
+
+}
+for (let i = 0; i < quests.length; i++){
+    const quest = quests[i];
+    renderLink(quest);
+}
