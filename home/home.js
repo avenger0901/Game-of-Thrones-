3b086112215a4fe56934dad15dd4216429334a79
zhao ziyi <zhaoziy@oregonstate.edu>
2020-01-30 08:33:08 -0800

    add race on home page

---
+++ home/home.js
@@ -0,0 +1,11 @@
+import { saveUser } from '../data/api.js';
+import makerUser from './home-makeUser.js';
+
+const userSignUp = document.getElementById('user-sign-up');
+
+userSignUp.addEventListener('submit', function(event){
+    event.preventDefault();
+    const formData = new FormData(userSignUp);
+    const user = makerUser(formData);
+    saveUser(user);
+});
