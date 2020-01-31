54ca5f583040fde7efcfbb3eeedd59af57dff896
zhao ziyi <zhaoziy@oregonstate.edu>
2020-01-31 08:43:07 -0800

    choice-result showing

--- quest/quest.js
+++ quest/quest.js
@@ -13,6 +13,7 @@ if (!quest) {
     window.location = '../map';
 }
 const form = document.getElementById('question-choices-container');
+const resultDescription = document.getElementById('result-description');
 const title = document.getElementById('title');
 title.textContent = quest.title;
 const img = document.getElementById('img');
@@ -22,19 +23,16 @@ const userChoices = document.getElementById('the-choices');
 img.src = '../assets/quest/' + quest.image;
 for (let i = 0; i < quest.choices.length; i++){
     const choice = quest.choices[i];
-    console.log(choice, 'choice');
     const choiceDom = createChoice(choice);
-    console.log(choiceDom, 'choicedom');
     userChoices.appendChild(choiceDom);
 }
-console.log(form);
 form.addEventListener('submit', function(event){
     event.preventDefault();
     const formData = new FormData(form);
     const getFormId = formData.get('choice');
     console.log(getFormId, '========');
-    const returnObject = findById(getFormId, quests);
+    const returnObject = findById(getFormId, quest.choices);
     console.log(returnObject);
-
+    resultDescription.textContent = returnObject.result;
 
 });
