fd18512e799d72939ec990d991e809463b350e1c
zhao ziyi <zhaoziy@oregonstate.edu>
2020-01-31 07:22:28 -0800

    add choices

--- quest/quest.js
+++ quest/quest.js
@@ -4,12 +4,9 @@ import { findById } from './findById.js';
 // // import { getUser } from '../utils/loadProfile.js';
 // loadProfile();
 const searchParam = new URLSearchParams(window.location.search);
-console.log(searchParam);
 const questId = searchParam.get('id');
 const quest = findById(questId, quests);
-console.log(questId);
-console.log(quest);
-console.log('test');
+
 if (!quest) {
     window.location = '../map';
 }
@@ -19,11 +16,25 @@ const form = document.getElementById('question-choices-container');
 const img = document.getElementById('img');
 const title = document.getElementById('title');
 const question = document.getElementById('questions');
-const ul = document.getElementById('the-choices');
+const userChoices = document.getElementById('the-choices');
 img.src = '../assets/quest/' + quest.image;
-console.log(img.src);
 question.textContent = quest.description;
-console.log(question.textContent);
-    //append those elements to <from id='question=choices>
-//render questions from questData
-//render choice from questData
+for (let i = 0; i < quest.choices.length; i++){
+    const choice = quest.choices[i];
+    const choiceDom = createChoice(choice);
+    userChoices.appendChild(choiceDom);
+}
+function createChoice(choice) {
+    const label = document.createElement('labal');
+    label.classList.add('choice');
+    const radio = document.createElement('input');
+    label.appendChild(radio);
+    radio.type = 'radio';
+    radio.name = 'choice';
+    radio.required = 'true';
+    radio.id = choice.id;
+    const description = document.createElement('span');
+    description.textContent = choice.description;
+    label.appendChild(description);
+    return label;
+}
